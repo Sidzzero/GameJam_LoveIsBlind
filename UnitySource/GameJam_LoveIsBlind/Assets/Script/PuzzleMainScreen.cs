3525f0db8ac3a81ba3519f639a61f30e267895a9
Sidz <srikanth.siddhu@gmail.com>
2019-02-23 08:45:54 +0530

    Base Flow Added

---
+++ UnitySource/GameJam_LoveIsBlind/Assets/Script/PuzzleMainScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Sidz.BGameJam
+{
+    public class PuzzleMainScreen : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #region UI
+
+        public void PlayGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        #endregion
+    }
+}
